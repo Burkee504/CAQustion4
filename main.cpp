e570f313f364e163246153708b558421599421c8
Burkee504 <oisburke1@gmail.com>
2024-07-13 16:24:22 +0000

    Commit Final

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "mbed.h"
+
+// Question 4 CA embedded systems. Add a+b store and print to d . Subtract c-d store and print E. 
+int main()
+{
+    int a = 10 ;
+    int b = 20 ;
+    int c = 30 ; 
+
+    int d = a + b; 
+    printf("Sum (d) = %d\n", d);
+
+    int e = d -c;
+    printf(" Sum (e) = %d\n", e);
+
+    while (1)
+
+    {
+        
+    }
+}
+
